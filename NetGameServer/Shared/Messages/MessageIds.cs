a779dc275eddbc97c248eb13c56920d3c994d582
长生 <jacke.yang@uccvr.com>
2026-05-21 08:50:56 +0800

    Update MessageIds.cs

--- NetGameServer/Shared/Messages/MessageIds.cs
+++ NetGameServer/Shared/Messages/MessageIds.cs
@@ -43,6 +43,17 @@ namespace Shared.Messages
         public const int DbResolveUserByUniqueIdReq = 1015;
         public const int DbResolveUserByUserIdReq = 1016;
 
+        // === DB 好友/聊天等响应消息 (1100-1199) ===
+        public const int DbAddFriendRes = 1106;
+        public const int DbRemoveFriendRes = 1107;
+        public const int DbSetFriendRemarkRes = 1108;
+        public const int DbGetFriendsRes = 1109;
+        public const int DbAddBlacklistRes = 1112;
+        public const int DbRemoveBlacklistRes = 1113;
+        public const int DbGetBlacklistRes = 1114;
+        public const int DbResolveUserByUniqueIdRes = 1115;
+        public const int DbResolveUserByUserIdRes = 1116;
+
         // === 登录服务器客户端通信 (10000-19999 网关转发过来) ===
         public const int PlayerDisconnectNotif = 10000;
 
@@ -152,16 +163,5 @@ namespace Shared.Messages
         public const int RemoveBlacklistRes = 50015;
         public const int GetBlacklistReq = 50016;
         public const int GetBlacklistRes = 50017;
-
-        // === DB 好友/聊天等响应消息 (1100-1199) ===
-        public const int DbAddFriendRes = 1106;
-        public const int DbRemoveFriendRes = 1107;
-        public const int DbSetFriendRemarkRes = 1108;
-        public const int DbGetFriendsRes = 1109;
-        public const int DbAddBlacklistRes = 1112;
-        public const int DbRemoveBlacklistRes = 1113;
-        public const int DbGetBlacklistRes = 1114;
-        public const int DbResolveUserByUniqueIdRes = 1115;
-        public const int DbResolveUserByUserIdRes = 1116;
     }
 }
